6dfd71ecc2bfcdf6ea55f17c5a31c354b0ca054e
mmheydari97 <heidarymm@yahoo.com>
2022-05-20 11:40:56 -0400

    add intermediate disc output

--- discriminators/PatchDiscriminator.py
+++ discriminators/PatchDiscriminator.py
@@ -6,37 +6,33 @@ class PatchDiscriminator(nn.Module):
     def __init__(self, in_channels=3, out_channel=3):
         super(PatchDiscriminator, self).__init__()
 
-        def discriminator_block(in_filters, out_filters, normalization=True):
+        def discriminator_block(self, in_filters, out_filters, normalization=True):
             """Returns downsampling layers of each discriminator block"""
             layers = [nn.Conv2d(in_filters, out_filters, 4, stride=2, padding=1)]
             if normalization:
                 layers.append(nn.InstanceNorm2d(out_filters))
             layers.append(nn.LeakyReLU(0.2, inplace=True))
+            layers[-1].register_forward_hook(self.add_intermediate_output)
             return layers
 
         self.model = nn.Sequential(
-            *discriminator_block(in_channels + out_channel, 64, normalization=False),
-            *discriminator_block(64, 128),
-            *discriminator_block(128, 256),
-            *discriminator_block(256, 512),
+            *discriminator_block(self, in_channels + out_channel, 64, normalization=False),
+            *discriminator_block(self, 64, 128),
+            *discriminator_block(self, 128, 256),
+            *discriminator_block(self, 256, 512),
             nn.ZeroPad2d((1, 0, 1, 0)),
             nn.Conv2d(512, 1, 4, padding=1, bias=False)
         )
 
     def forward(self, img_A, img_B):
-        # Concatenate image and condition image by channels to produce input
+        # Concatenate image and condition image by channels to produce input     
+        self.intermediate_outputs = []
         img_input = torch.cat((img_A, img_B), 1)
         return self.model(img_input)
 
-    def intermediate_layer(self):
-        pass
+    def add_intermediate_output(self, lay, inp, outp):
+        self.intermediate_outputs.append(torch.autograd.Variable(outp.data, requires_grad=False))
+		
+    def get_intermediate_output(self):
+        return self.intermediate_outputs[:4]
 
-if __name__ == "__main__":
-    disc = PatchDiscriminator(3, 4)
-    a = torch.randn(1, 3, 256, 256)
-    b = torch.randn(1, 4, 256, 256)
-    out = disc(a, b)
-    for module in disc.children():
-        for layer in module.children():
-            if isinstance(layer, nn.Conv2d):
-                print(layer.weight.shape)
