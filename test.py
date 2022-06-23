c46aab38cd384df457da2eb6bd9d2d1ecac19bc1
mmheydari97 <heidarymm@yahoo.com>
2022-06-23 12:18:47 -0400

    test update

--- test.py
+++ test.py
@@ -7,10 +7,11 @@ import matplotlib.image
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
-from PIL import Image
+from PIL import Image, ImageFilter
 from PIL.ImageOps import invert
-import torch
+from tqdm import tqdm
 
+import torch
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
 from torchvision.transforms import ToTensor, ToPILImage
@@ -78,11 +79,24 @@ def visualize(out):
     
     ax.paste(grid, (0,0), grid_msk)
     ax.paste(content, (0,0), content_msk)
-    return ToTensor()(ax)
+    # ax = ax.filter(ImageFilter.MedianFilter(size=1)) meh
+    ax = ax.filter(ImageFilter.DETAIL)
+    # ax.save(f"./Outputs/pil/{np.random.randint(1,10000)}.png")
+    return ax
 
 
-def concat_images(photo,sketch,output):
-    return torch.cat((photo,sketch,output),2)
+def concat_images(*photos, mode="h"):
+    #torch.cat((photo,sketch,output),2)
+    if mode=="h":
+        res = Image.new(photos[0].mode, (photos[0].width*len(photos),photos[0].height))
+        for i in range(len(photos)):
+            res.paste(photos[i], (photos[i].width*i,0))
+    else:
+        res = Image.new(photos[0].mode, (photos[0].width, photos[0].height*len(photos)))
+        for i in range(len(photos)):
+            res.paste(photos[i], (0, photos[i].height*i))
+
+    return res
 
 def save_plot(loss_dict, opt):
 	x = np.array(range(opt.epoch_count, opt.epoch_count+opt.total_iters))
@@ -130,14 +144,16 @@ def eval_model(model, dataset, path):
         a_grid = (cm_grid[1,1]+cm_grid[0,0])/np.sum(cm_grid)
         a_cont = (cm_cont[1,1]+cm_cont[0,0])/np.sum(cm_cont)
         accuracy.append((a_axis+a_grid+a_cont)/3)
-        
-    print(f"Pixel Accuracy => min:{np.min(accuracy)}, max:{np.max(accuracy)}, avg:{np.mean(accuracy)}, std:{np.std(accuracy)}")
-    print(f"Dice Coeff => min:{np.min(dice)}, max:{np.max(dice)}, avg:{np.mean(dice)}, std:{np.std(dice)}")
-    print(f"Jaccard Index => min:{np.min(jaccard)}, max:{np.max(jaccard)}, avg:{np.mean(jaccard)}, std:{np.std(jaccard)}")
-    return {"Acc": np.mean(accuracy), "IoU": np.mean(jaccard), "Dice": np.mean(dice)}
+    
+    a = f"Pixel Accuracy => min:{np.min(accuracy)}, max:{np.max(accuracy)}, avg:{np.mean(accuracy)}, std:{np.std(accuracy)}\n"
+    d = f"Dice Coeff => min:{np.min(dice)}, max:{np.max(dice)}, avg:{np.mean(dice)}, std:{np.std(dice)}\n"
+    j = f"Jaccard Index => min:{np.min(jaccard)}, max:{np.max(jaccard)}, avg:{np.mean(jaccard)}, std:{np.std(jaccard)}\n"
+    with open(os.path.join(os.getcwd(),"models",opt.folder_load,"eval.txt"), 'w') as f:
+        f.writelines([a,d,j])
+    print (f"Acc: {np.mean(accuracy)}, IoU: {np.mean(jaccard)}, Dice: {np.mean(dice)}")
 
 def save_images(model, dataset, path):
-    for i, batch in enumerate(dataset):
+    for i, batch in enumerate(tqdm(dataset)):
         real_A, real_B = batch[0], batch[1]
         with torch.no_grad():
             out = model(real_A.to(device)).cpu()
@@ -161,12 +177,16 @@ def save_images(model, dataset, path):
         
         b_img = visualize(b)
         out_img = visualize(out)
-        save_image(concat_images(a, b_img, out_img), os.path.join(path,f"e_{i+1}.png")) 
-        b_elements = concat_images(torch.unsqueeze(b[0],0), torch.unsqueeze(b[1],0), torch.unsqueeze(b[2],0))
-        out_elements = concat_images(torch.unsqueeze(out[0],0), torch.unsqueeze(out[1],0), torch.unsqueeze(out[2],0))
-        save_image(torch.cat((b_elements, out_elements), 1), os.path.join(path,f"e_{i+1}_elements.png")) 
         
-        print(f"file e_{i+1}.png saved.")
+        out_img.save(os.path.join(path,f"o_{i+1}.png"))
+        concat_images(ToPILImage()(a), b_img, out_img).save(os.path.join(path,f"sgt_{i+1}.png"))
+        # save_image(concat_images(a, b_img, out_img), os.path.join(path,f"e_{i+1}.png"))
+        # save_image(torch.cat((b_elements, out_elements), 1), os.path.join(path,f"e_{i+1}_elements.png")) 
+        
+        b_elements = concat_images(ToPILImage()(b[0]), ToPILImage()(b[1]), ToPILImage()(b[2]))
+        out_elements = concat_images(ToPILImage()(out[0]), ToPILImage()(out[1]), ToPILImage()(out[2]))
+        concat_images(b_elements,out_elements, mode="v").save(os.path.join(path,f"elm_{i+1}.png"))
+        # print(f"file x_{i+1}.png saved.")
 
 
 if __name__=='__main__':
